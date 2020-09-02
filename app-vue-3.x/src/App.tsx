98059fa597f7e65e1ace9d8ccee7115495314151
xinglong-zh <xinglong_zh@foxmail.com>
2020-09-02 18:23:36 +0800

    replace vue with tsx

---
+++ app-vue-3.x/src/App.tsx
@@ -0,0 +1,15 @@
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
+
+export default defineComponent({
+  name: 'App',
+  render() {
+    return (
+      <div id="app">
+        <RouterView></RouterView>
+      </div>
+    )
+  }
+})
+
+
--- app-vue-3.x/src/router/index.ts
+++ app-vue-3.x/src/router/index.ts
@@ -1,35 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path:'/type',
-    name:'Type',
-    component:()=> import('../views/Type')
-  },
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // },
+  // {
+  //   path:'/type',
+  //   name:'Type',
+  //   component:()=> import('../views/Type')
+  // },
   {
     path:'/bilibili',
     name:'bilibili',
     component:()=> import('@/views/bilibili/index'),
   },
-  {
-    path:'/icon',
-    name:'icon',
-    component:() => import('@/icon/index')
-  }
+  // {
+  //   path:'/icon',
+  //   name:'icon',
+  //   component:() => import('@/icon/index')
+  // }
 ]
 
 const router = createRouter({
