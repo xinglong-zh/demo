59699f1b6251f66cf1ba52607ed60eaaf880b4c7
xinglong-zh <xinglong_zh@foxmail.com>
2021-04-03 19:01:09 +0800

    vite app demo

--- app-vue-3.x/src/views/demo/promise.js
+++ app-vue-3.x/src/views/demo/promise.js
@@ -1,2 +1,14 @@
 // 自定实现promise
 
+
+// https://www.promisejs.org/implementing/
+var PENDING  = '0'
+var FULFILLED = '1'
+var REJECTED = '2'
+
+// 状态机
+
+function Promise() {
+    this.state = PENDING; // 初始状态
+    
+}
