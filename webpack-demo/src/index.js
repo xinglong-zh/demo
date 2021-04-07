f907deddf4dd359a35fa64706655aa839e19296b
xinglong-zh <xinglong_zh@foxmail.com>
2021-04-07 23:52:47 +0800

    todo custom cli and   prod.js

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,2 +1,9 @@
+import './style.css'
+import printMe from './print'
+import _ from 'loadsh'
+
 console.log('hello world');
+printMe();
+// console.log(process.env)
 // https://webpack.docschina.org/guides/getting-started/
+
---
+++ webpack-demo/src/print.js
@@ -0,0 +1,4 @@
+import _ from 'loadsh'
+export default function printMe(){
+    console.log('print me ');
+}
