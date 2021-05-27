99ce865c8f444636a2be34b4be0f0f6c716a93e5
xinglong-zh <xinglong_zh@foxmail.com>
2021-05-27 22:32:04 +0800

    canvas marker 踩坑

--- app-vue-3.x/src/views/map/index.vue
+++ app-vue-3.x/src/views/map/index.vue
@@ -6,107 +6,69 @@
 
 <script>
 import * as L from "leaflet";
-import * as topojson from "topojson";
+// import * as topojson from "topojson";
 // import '@/assets/js/bundle';
 //extend Leaflet to create a GeoJSON layer from a TopoJSON file
-L.TopoJSON = L.GeoJSON.extend({
-  addData: function (data) {
-    var geojson, key;
-    if (data.type === "Topology") {
-      for (key in data.objects) {
-        // eslint-disable-next-line no-prototype-builtins
-        if (data.objects.hasOwnProperty(key)) {
-          geojson = topojson.feature(data, data.objects[key]);
-          L.GeoJSON.prototype.addData.call(this, geojson);
-        }
-      }
-      return this;
-    }
-    L.GeoJSON.prototype.addData.call(this, data);
-    return this;
-  },
-});
-L.topoJson = function (data, options) {
-  return new L.TopoJSON(data, options);
-};
+// L.TopoJSON = L.GeoJSON.extend({
+//   addData: function (data) {
+//     var geojson, key;
+//     if (data.type === "Topology") {
+//       for (key in data.objects) {
+//         // eslint-disable-next-line no-prototype-builtins
+//         if (data.objects.hasOwnProperty(key)) {
+//           geojson = topojson.feature(data, data.objects[key]);
+//           L.GeoJSON.prototype.addData.call(this, geojson);
+//         }
+//       }
+//       return this;
+//     }
+//     L.GeoJSON.prototype.addData.call(this, data);
+//     return this;
+//   },
+// });
+// L.topoJson = function (data, options) {
+//   return new L.TopoJSON(data, options);
+// };
 
-// 自定义tile
-L.GridLayer.DebugCoords = L.GridLayer.extend({
-  createTile: function (coords, done) {
-    var tile = document.createElement("div");
-    tile.innerHTML = [coords.x, coords.y, coords.z].join(", ");
-    tile.style.outline = "1px solid red";
+// // 自定义tile
+// L.GridLayer.DebugCoords = L.GridLayer.extend({
+//   createTile: function (coords, done) {
+//     var tile = document.createElement("div");
+//     tile.innerHTML = [coords.x, coords.y, coords.z].join(", ");
+//     tile.style.outline = "1px solid red";
 
-    setTimeout(function () {
-      done(null, tile); // Syntax is 'done(error, tile)'
-    }, 500 + Math.random() * 1500);
+//     setTimeout(function () {
+//       done(null, tile); // Syntax is 'done(error, tile)'
+//     }, 500 + Math.random() * 1500);
 
-    return tile;
-  },
-});
-L.gridLayer.debugCoords = function (opts) {
-  return new L.GridLayer.DebugCoords(opts);
-};
+//     return tile;
+//   },
+// });
+// L.gridLayer.debugCoords = function (opts) {
+//   return new L.GridLayer.DebugCoords(opts);
+// };
 
-L.Circle.Demo = L.Circle.extend({
-  getEvens:function(){
-    var events = L.Circle.prototype.getEvents.call(this);
-    events.moveend = this.redraw 
-  }
-})
+// L.Circle.Demo = L.Circle.extend({
+//   getEvens: function () {
+//     var events = L.Circle.prototype.getEvents.call(this);
+//     events.moveend = this.redraw;
+//   },
+// });
 
-L.circle.demo = function(opts){
-  return new L.Circle.Demo(opts);
-}
+// L.circle.demo = function (opts) {
+//   return new L.Circle.Demo(opts);
+// };
 
-// 自定义canvas
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
-L.CustomLayer = (L.Layer ? L.Layer:L.Class).extend({
-  options:{
-    pane:'markerPane'
-  },
-  onAdd:function(map){
-    let pane = map.getPane(this.options.pane);
-    this._container = L.DomUtil.create('canvas');
-    L.DomUtil.addClass(this._container,'marker-canvas')
-    const {x,y} = map.getSize();
-    this._container.width = x;
-    this._container.height = y;
-    pane.appendChild(this._container);    // bounds.min 左上角
+// import ContourLayerExt from '@/assets/js/contour/ContourLayerExt'
+import "@/assets/js/leaflet.canvas-markers.js";
 
-  },
-  addMarkers:function(markers){
-    console.log(markers);
-  },
-  addMarker:function(marker){
-    let latlng = marker.getLatLng();
-    
-  },
-  onRemove:function(map){
-      L.DomUtil.remove(this._container);
-      map.off('zoomend viewreset',this._update,this);
-  },
-  _update:function(){
+import Axios from "axios";
 
-    let bounds = this._bounds;
-    L.Util.setOptions(this._container,bounds.min);
-  }
-})
-
-L.customLayer = function(opts){
-  return new L.CustomLayer(opts);
-}
-
-// import {canvasLayer} from '@/assets/js/contour/L.CanvasLayer'
-
-// import ContourLayerExt from '@/assets/js/contour/ContourLayerExt' 
-
-
-
-import Axios from 'axios';
-
-import BinLayer from '@/assets/js/BinLayer'
-import { Meta1 } from '@/assets/js/gl-shaded/Catalog';
+import BinLayer from "@/assets/js/BinLayer";
+import { Meta1 } from "@/assets/js/gl-shaded/Catalog";
 
 export default {
   name: "demoMap",
@@ -123,22 +85,30 @@ export default {
         zoomDelta: 0.5,
         zoom: 2,
         attributionControl: false,
-        worldCopyJump:false,
+        worldCopyJump: false,
       }),
     },
   },
   methods: {
     async initMap() {
-      let map = L.map("map",{worldCopyJump:false,preferCanvas:true,renderer:L.canvas({tolerance:10})},).setView([100, -0.09], 3);
+      let DefaultIcon = L.icon({
+        iconUrl: icon,
+        shadowUrl: iconShadow,
+        iconAnchor: [12, 41], // 配置icon的偏移   png:21*41
+        iconSize: [25, 41],
+      });
+      L.Marker.prototype.options.icon = DefaultIcon;
+      let map = L.map("map", {
+        worldCopyJump: false,
+        preferCanvas: true,
+        renderer: L.canvas({ tolerance: 10 }),
+      }).setView([59, 30], 10);
 
-      // let res = await Axios.get('./response.json');
-      console.log(map.getSize());
-      let c =  L.customLayer({});
-      map.addLayer(c);
-      
-      
+      let marker = L.marker(map.getCenter(), { icon: DefaultIcon });
 
-    }
+      var ciLayer = L.canvasIconLayer({}).addTo(map);
+      ciLayer.addMarker(marker);
+    },
   },
   mounted() {
     this.initMap();
@@ -166,19 +136,19 @@ export default {
 }
 
 #info {
-	position:absolute; 
-	top:0; 
-	right:0; 
-	width: 20em; 
-	height: 7.5em; 
-	background: rgba(255,255,255,.5); 
-	z-index:500; 
-	font: 12px Sans;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 20em;
+  height: 7.5em;
+  background: rgba(255, 255, 255, 0.5);
+  z-index: 500;
+  font: 12px Sans;
 }
 
 .crsMarker {
-	border-top: 2px green solid;
-	border-left: 2px green solid;
+  border-top: 2px green solid;
+  border-left: 2px green solid;
 }
 .marker-canvas {
   width: 100%;
